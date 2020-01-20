e1722143f56bf8e8377e1b60a517aa4afe568e7e
Souque <guillaume.souque@polymtl.ca>
2020-01-20 16:00:37 -0500

    (D) Ajout du component guide

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
+import { GuideComponent } from './guide/guide.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, GuideComponent],
     imports: [BrowserModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
