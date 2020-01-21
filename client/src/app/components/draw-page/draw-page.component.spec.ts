9355b7ee0438bbbd3b0726ea09b18bb23235b1e6
Seif Sarsar <seifsarsar@hotmail.com>
2020-01-20 21:22:01 -0500

    Page dessin initial commit

--- client/src/app/components/draw-page/draw-page.component.spec.ts
+++ client/src/app/components/draw-page/draw-page.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DrawPageComponent } from './draw-page.component';
 
 describe('DrawPageComponent', () => {
-  let component: DrawPageComponent;
-  let fixture: ComponentFixture<DrawPageComponent>;
+    let component: DrawPageComponent;
+    let fixture: ComponentFixture<DrawPageComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DrawPageComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [DrawPageComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DrawPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DrawPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
