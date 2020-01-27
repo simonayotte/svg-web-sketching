8b73856c0759a7e1f2a9cf96f80c0832d4660905
hichem <hichem.benmessaoud@polymtl.ca>
2020-01-26 23:00:36 -0500

    Formulaire de creation d'un nouveau dessin (D)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'ngx-color-picker';
+
 
 import { routes } from '../environments/routes';
 
 import { DrawPageComponent } from '../app/components/draw-page/draw-page.component';
+import { CreateDrawingComponent } from '../app/components/create-drawing/create-drawing.component';
 @NgModule({
-    declarations: [AppComponent, DrawPageComponent],
-    imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
+    declarations: [AppComponent, DrawPageComponent, CreateDrawingComponent],
+    imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes),ReactiveFormsModule,FormsModule,ColorPickerModule],
     providers: [],
     bootstrap: [AppComponent],
 })
