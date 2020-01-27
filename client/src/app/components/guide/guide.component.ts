df3be5877c9e0ada5852fbfb5a5f90b168cd2630
Souque <guillaume.souque@polymtl.ca>
2020-01-26 22:55:32 -0500

    (D) Ajout de la mise a jour de la liste des catégories après un changement de sujet

--- client/src/app/components/guide/guide.component.ts
+++ client/src/app/components/guide/guide.component.ts
@@ -16,8 +16,6 @@ export class GuideComponent implements OnInit {
     this.displayTextSubject(this.currentText);
   }
 
-
-
   collapse(category_id:string) {
     let category = document.getElementById(category_id);
     let list = document.getElementById(category_id + "-list");
@@ -36,9 +34,28 @@ export class GuideComponent implements OnInit {
       document.getElementsByClassName("subject-text")[i].setAttribute("style","none");
     }
     let newText = document.getElementById("page-" + subject_id);
-    if (newText) newText.style.display="block";
-    
-    
+    if (newText) {
+      newText.style.display="block";
+      this.currentText = subject_id;
+    }
+
+    this.resetContentTable();
+    this.updateContentTable()
   }
 
+  updateContentTable() {
+    let categories = document.getElementsByClassName("category-items");
+    for (let i = 0; i < categories.length; i++) {
+      for (let j = 0; j < categories[i].children.length; j++) {
+        if (categories[i].children[j].children[0].id == this.currentText) document.getElementById(categories[i].id)!.style.display = "block";
+      }
+    }
+  }
+
+  resetContentTable() {
+    let categories = document.getElementsByClassName("category-items");
+    for (let i = 0; i < categories.length; i++) {
+      if (document.getElementById(categories[i].id) != null) document.getElementById(categories[i].id)!.style.display = "none";
+    }
+  }
 }
