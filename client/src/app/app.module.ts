0f54233ee121a4a2fa5ebf79b8691544a3fdebfc
Tarik <tarik.agday@polymtl.ca>
2020-01-27 14:59:54 -0500

    (D) Homepage fonctionel

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,8 +11,9 @@ import { routes } from '../environments/routes';
 
 import { DrawPageComponent } from '../app/components/draw-page/draw-page.component';
 import { CreateDrawingComponent } from '../app/components/create-drawing/create-drawing.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 @NgModule({
-    declarations: [AppComponent, DrawPageComponent, CreateDrawingComponent],
+    declarations: [AppComponent, DrawPageComponent, CreateDrawingComponent, HomePageComponent],
     imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes),ReactiveFormsModule,FormsModule,ColorPickerModule],
     providers: [],
     bootstrap: [AppComponent],
