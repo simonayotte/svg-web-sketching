4de0ba28ecdbc9358fe5cea50794c00ee6ffb7d8
Souque <guillaume.souque@polymtl.ca>
2020-02-02 21:35:07 -0500

    (D) Implémentation de la création de carrés de base

--- client/src/app/services/rectangle/rectangle.service.ts
+++ client/src/app/services/rectangle/rectangle.service.ts
@@ -20,13 +20,17 @@ export class RectangleService implements OnInit, OnDestroy {
     this.drawStateService.currentColorObs.subscribe((color: string) => (this.color = color));
 
     //Bind this to event listeners
-    this.mouseDownListener = this.startDraw.bind(this);
-    this.mouseMoveListener = this.continueDraw.bind(this);
-    this.mouseUpListener = this.stopDraw.bind(this);
-    this.mouseOutListener = this.stopDraw.bind(this);
+    this.mouseDownListener = this.startRect.bind(this);
+    this.mouseMoveListener = this.continueRect.bind(this);
+    this.mouseUpListener = this.stopRect.bind(this);
+    this.mouseOutListener = this.stopRect.bind(this);
   }
   ngOnInit() {
       this.canvasRef.nativeElement.addEventListener('mousedown', this.mouseDownListener);
+      /// TODO: get canvas width and height
+      this.canvasHeight = 2000;
+      this.canvasWidth = 2000;
+      this.canvasImage = this.canvasContext.getImageData(0,0,this.canvasWidth,this.canvasHeight);
   }
 
   ngOnDestroy() {
@@ -51,44 +55,52 @@ export class RectangleService implements OnInit, OnDestroy {
   private color: string;
   private isPanelOpen: boolean;
 
-  private lastX: number;
-  private lastY: number;
+  private initialX: number;
+  private initialY: number;
 
-  startDraw(event: MouseEvent): void {
-      let positionX = this.isPanelOpen ? event.clientX - 252 : event.clientX - 52;
+  private isDrawing: boolean;
+  private canvasImage: ImageData;
+  private canvasHeight: number;
+  private canvasWidth: number;
 
-      //Stroke style
-      this.canvasContext.lineJoin = 'round';
-      this.canvasContext.lineCap = 'round';
-      this.canvasContext.lineWidth = this.thickness.value;
-      this.canvasContext.strokeStyle = this.color;
 
-      //Write circle when click only
-      this.canvasContext.beginPath();
-      this.canvasContext.arc(positionX, event.clientY, this.thickness.value / 2, 0, 2 * Math.PI);
-      this.canvasContext.closePath();
-      this.canvasContext.fill();
-      this.lastX = positionX;
-      this.lastY = event.clientY;
+  startRect(event: MouseEvent): void {
+      if (!this.isDrawing) {
+        this.isDrawing = true;
+        let positionX = this.isPanelOpen ? event.clientX - 252 : event.clientX - 52;
 
-      this.canvasRef.nativeElement.addEventListener('mousemove', this.mouseMoveListener);
-      this.canvasRef.nativeElement.addEventListener('mouseup', this.mouseUpListener);
-      this.canvasRef.nativeElement.addEventListener('mouseout', this.mouseOutListener);
+        this.initialX = positionX;
+        this.initialY = event.clientY;
+
+        //Stroke style
+        
+        this.canvasContext.lineJoin = 'miter';
+        this.canvasContext.lineCap = 'square';
+        this.canvasContext.lineWidth = this.thickness.value;
+        this.canvasContext.strokeStyle = this.color;
+
+        this.canvasRef.nativeElement.addEventListener('mousemove', this.mouseMoveListener);
+        this.canvasRef.nativeElement.addEventListener('mouseup', this.mouseUpListener);
+        this.canvasRef.nativeElement.addEventListener('mouseout', this.mouseOutListener);
+      }
   }
 
-  continueDraw(event: MouseEvent): void {
+  continueRect(event: MouseEvent): void {
       let positionX = this.isPanelOpen ? event.clientX - 252 : event.clientX - 52;
 
+      this.canvasContext.clearRect(0,0,this.canvasWidth,this.canvasHeight);
+      this.canvasContext.putImageData(this.canvasImage,0,0);
+
       this.canvasContext.beginPath();
-      this.canvasContext.moveTo(this.lastX, this.lastY);
-      this.canvasContext.lineTo(positionX, event.clientY);
-      this.canvasContext.closePath();
+      let width = positionX - this.initialX;
+      let height = event.clientY - this.initialY;
+      this.canvasContext.rect(this.initialX,this.initialY,width,height);
       this.canvasContext.stroke();
-      this.lastX = positionX;
-      this.lastY = event.clientY;
   }
 
-  stopDraw(): void {
+  stopRect(): void { 
+      this.canvasImage = this.canvasContext.getImageData(0,0,this.canvasWidth,this.canvasHeight);
+      this.isDrawing = false;
       this.canvasRef.nativeElement.removeEventListener('mousemove', this.mouseMoveListener);
       this.canvasRef.nativeElement.removeEventListener('mouseup', this.mouseUpListener);
       this.canvasRef.nativeElement.removeEventListener('mouseout', this.mouseOutListener);
