bf5a0c96ea5cc5a097553d787863165ec67138ca
Souque <guillaume.souque@polymtl.ca>
2020-02-08 16:47:47 -0500

    (R) Lint de guide

--- client/src/app/components/guide/guide.component.ts
+++ client/src/app/components/guide/guide.component.ts
@@ -1,42 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-guide',
   templateUrl: './guide.component.html',
   styleUrls: ['./guide.component.scss']
 })
-export class GuideComponent implements OnInit { 
-
-  currentText = "bienvenue";
-  constructor() {
-  }
+export class GuideComponent implements OnInit {
+  currentText = 'bienvenue';
 
   ngOnInit() {
     this.displayTextSubject(this.currentText);
   }
 
-  collapse(category_id:string) {
-    let category = document.getElementById(category_id);
-    let list = document.getElementById(category_id + "-list");
-    if (category != null && list != null){
-      if (list.style.display === "block"){
-        list.style.display = "none";
-      }
-      else {
-        list.style.display = "block";
+  collapse(categoryId: string) {
+    const category = document.getElementById(categoryId);
+    const list = document.getElementById(categoryId + '-list');
+    if (category != null && list != null) {
+      if (list.style.display === 'block') {
+        list.style.display = 'none';
+      } else {
+        list.style.display = 'block';
       }
     }
   }
 
-  displayTextSubject(subject_id:string) {
-    for (let i = 0; i < document.getElementsByClassName("subject-text").length; i++){
-      document.getElementsByClassName("subject-text")[i].setAttribute("style","none");
+  // impossible to do a 'for-of' loop, HTMLCollectionOf<Element> doesn't have a [Symbol.iterator]() method
+  /*tslint:disable:prefer-for-of*/
+  displayTextSubject(subjectId: string) {
+    for (let i = 0; i < document.getElementsByClassName('subject-text').length; i++) {
+      document.getElementsByClassName('subject-text')[i].setAttribute('style', 'none');
     }
-    let newText = document.getElementById("page-" + subject_id);
+    const newText = document.getElementById('page-' + subjectId);
     if (newText) {
-      newText.style.display="block";
-      this.currentText = subject_id;
+      newText.style.display = 'block';
+      this.currentText = subjectId;
     }
 
     this.resetContentTable();
@@ -44,18 +41,22 @@ export class GuideComponent implements OnInit {
   }
 
   updateContentTable() {
-    let categories = document.getElementsByClassName("category-items");
+    const categories = document.getElementsByClassName('category-items');
     for (let i = 0; i < categories.length; i++) {
       for (let j = 0; j < categories[i].children.length; j++) {
-        if (categories[i].children[j].children[0].id == this.currentText) document.getElementById(categories[i].id)!.style.display = "block";
+        if (categories[i].children[j].children[0].id === this.currentText && document.getElementById(categories[i].id) != null) {
+          document.getElementById(categories[i].id)!.style.display = 'block'; // tslint:disable-line:no-non-null-assertion
+        }
       }
     }
   }
 
   resetContentTable() {
-    let categories = document.getElementsByClassName("category-items");
+    const categories = document.getElementsByClassName('category-items');
     for (let i = 0; i < categories.length; i++) {
-      if (document.getElementById(categories[i].id) != null) document.getElementById(categories[i].id)!.style.display = "none";
+      if (document.getElementById(categories[i].id) != null) {
+        document.getElementById(categories[i].id)!.style.display = 'none'; // tslint:disable-line:no-non-null-assertion
+      }
     }
   }
 }
