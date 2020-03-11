b6c392769e6af5ed28dee5a64750278d189bd360
hichembenm <you@example.com>
2020-03-11 00:29:42 -0400

    le preview de limage a sauvegarder respecte les proportions du canvas (D)

--- client/src/app/components/dialogs/preview-image/preview-image.component.spec.ts
+++ client/src/app/components/dialogs/preview-image/preview-image.component.spec.ts
@@ -1,16 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { PreviewImageComponent } from './preview-image.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
+import { DrawStore } from 'src/app/store/draw-store';
 
 describe('PreviewImageComponent', () => {
   let component: PreviewImageComponent;
   let fixture: ComponentFixture<PreviewImageComponent>;
+  const dialogMock = {
+    close: () => {
+        /*empty function*/
+    },
+};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PreviewImageComponent ]
-    })
-    .compileComponents();
+        declarations: [PreviewImageComponent],
+        imports: [FormsModule, ReactiveFormsModule],
+        providers: [
+            {
+                provide: MatDialogRef,
+                useValue: { dialogMock },
+            },
+            DrawStore
+        ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +37,17 @@ describe('PreviewImageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('#setPreviewImgDimension should set #previewWidth to be equal to the canvas width if the canvas width is smaller than 300 px', async(inject([DrawStore], (drawStore: DrawStore) => {
+    drawStore.setCanvasWidth(150);
+    component.setPreviewImgDimension()
+    expect(component.previewWidth).toBe(150);
+  })));
+
+  it('#setPreviewImgDimension should set #previewHeight to be equal to the canvas height if the canvas width is smaller than 300 px', async(inject([DrawStore], (drawStore: DrawStore) => {
+    drawStore.setCanvasHeight(200);
+    component.setPreviewImgDimension()
+    expect(component.previewHeight).toBe(200);
+  })));
+  
 });
--- client/src/app/components/dialogs/preview-image/preview-image.component.ts
+++ client/src/app/components/dialogs/preview-image/preview-image.component.ts
@@ -4,15 +4,20 @@ import { DrawStore } from 'src/app/store/draw-store';
 import { DrawState } from 'src/app/state/draw-state';
 import { MatDialogRef } from '@angular/material';
 
+const DEFAULT_IMG_PREVIEW_SIZE:number = 300;
+
 @Component({
   selector: 'app-preview-image',
   templateUrl: './preview-image.component.html',
   styleUrls: ['./preview-image.component.scss']
 })
+
 export class PreviewImageComponent implements OnInit {
   dataURL: string;
   state:DrawState;
   buttonDisabled = false;
+  previewHeight:number;
+  previewWidth:number;
   constructor(private saveDrawingService:SaveDrawingService, private store:DrawStore, private dialogRef:MatDialogRef<PreviewImageComponent>) { 
     this.saveDrawingService.dataURLObs.subscribe((dataURL: string) => (this.dataURL = dataURL));
 
@@ -21,11 +26,28 @@ export class PreviewImageComponent implements OnInit {
   });
   }
   ngOnInit() {
+    this.setPreviewImgDimension();
     this.state.globalState.isKeyHandlerActive = false;
   }
 
   ngOnDestroy() {
     this.state.globalState.isKeyHandlerActive = true;
+  }
+
+  setPreviewImgDimension() {
+    if(this.state.canvasState.canvas.width < DEFAULT_IMG_PREVIEW_SIZE){
+      this.previewWidth = this.state.canvasState.canvas.width
+    }
+    else if(this.state.canvasState.canvas.height < DEFAULT_IMG_PREVIEW_SIZE){
+      this.previewWidth = this.state.canvasState.canvas.width
+    }
+    else{
+      let width_height_ratio = this.state.canvasState.canvas.width/this.state.canvasState.canvas.height;
+      width_height_ratio > 1? (this.previewWidth = DEFAULT_IMG_PREVIEW_SIZE 
+                              , this.previewHeight = DEFAULT_IMG_PREVIEW_SIZE/width_height_ratio):
+                              (this.previewHeight = DEFAULT_IMG_PREVIEW_SIZE 
+                              , this.previewWidth = DEFAULT_IMG_PREVIEW_SIZE*width_height_ratio);
+        }
   }
 
   saveDrawing() {
