7aaddc25d6fb2fa8cba1935e9d2c0ea013f4c16b
hichembenm <you@example.com>
2020-04-02 14:52:41 -0400

    QA de tools

--- client/src/app/models/tool.ts
+++ client/src/app/models/tool.ts
@@ -4,6 +4,12 @@ export abstract class Tool {
     protected mouseMoveListener: EventListener;
 
     svg: SVGGraphicsElement;
+    start(event: MouseEvent) {}
+    continue(event: MouseEvent) {}
+    stop() {}
+    stopSignal() {} // for testing purpose
+    handleKeyDown(key: string) {}
+    handleKeyUp(key: string) {}
 
     copyState(svgs: SVGGraphicsElement[]): SVGGraphicsElement[] {
         let newSvgs = [];
