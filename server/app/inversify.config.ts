f31cee35c98e57c1d26441c03b98b14110695b6c
hichembenm <you@example.com>
2020-04-03 20:46:44 -0400

    nouvelle version de inversify.config

--- server/app/inversify.config.ts
+++ server/app/inversify.config.ts
@@ -1,30 +1,32 @@
-import { Container } from 'inversify';
+
 import { Application } from './app';
-import { DateController } from './controllers/date.controller';
-import { IndexController } from './controllers/index.controller';
-import { Server } from './server';
+import { Container } from 'inversify';
+import { DatabaseService} from '../app/services/DB.service'
+import { DateController } from '../app/controllers/date.controller';
 import { DateService } from './services/date.service';
+import { ExportDrawingController } from '../app/controllers/exportdrawing.controller';
+import { FileHandler} from '../app/services/file-handler.service'
+import { GalleryController } from '../app/controllers/gallery.controller';
+import { IndexController } from '../app/controllers/index.controller';
 import { IndexService } from './services/index.service';
+import { SaveDrawingController } from '../app/controllers/savedrawing.controller';
+import { Server } from './server';
 import Types from './types';
-import { SaveDrawingController } from './controllers/savedrawing.controller';
-import { FileHandler } from './services/file-handler.service';
-import { DatabaseService } from './services/DB.service';
-import { ExportDrawingController } from './controllers/exportdrawing.controller';
-import { GalleryController } from './controllers/gallery.controller';
 
-const container: Container = new Container();
+export const containerBootstrapper: () => Promise<Container> = async () => {
+    const container: Container = new Container();
 
-container.bind(Types.Server).to(Server);
-container.bind(Types.Application).to(Application);
-container.bind(Types.IndexController).to(IndexController);
-container.bind(Types.IndexService).to(IndexService);
-container.bind(Types.DateController).to(DateController);
-container.bind(Types.DateService).to(DateService);
-container.bind(Types.SaveDrawingController).to(SaveDrawingController);
-container.bind(Types.ExportDrawingController).to(ExportDrawingController);
-container.bind(Types.GalleryController).to(GalleryController);
-container.bind(Types.FileHandler).to(FileHandler);
-container.bind(Types.DatabaseService).to(DatabaseService);
+    container.bind(Types.Server).to(Server);
+    container.bind(Types.Application).to(Application);
+    container.bind(Types.IndexController).to(IndexController);
+    container.bind(Types.IndexService).to(IndexService);
+    container.bind(Types.DateController).to(DateController);
+    container.bind(Types.DateService).to(DateService);
+    container.bind(Types.ExportDrawingController).to(ExportDrawingController);
+    container.bind(Types.SaveDrawingController).to(SaveDrawingController);
+    container.bind(Types.FileHandler).to(FileHandler);
+    container.bind(Types.GalleryController).to(GalleryController);
+    container.bind(Types.DatabaseService).to(DatabaseService);
 
-
-export { container };
+    return container;
+};
