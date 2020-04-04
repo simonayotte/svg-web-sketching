117486a49ce0cc0fcf92657cfb472f287a04af91
hichembenm <you@example.com>
2020-04-03 20:40:46 -0400

    test serveurs

---
+++ server/app/controllers/gallery.controller.spec.ts
@@ -0,0 +1,63 @@
+import { expect } from 'chai';
+import * as supertest from 'supertest';
+import { Stubbed, testingContainer } from '../../../server/test/test-utils';//Stubbed,Stubbed,
+import { Application } from '../app';
+import { FileHandler } from '../services/file-handler.service';
+import Types from '../types';
+//import { DatabaseService } from '../services/DB.service';
+import { Drawing} from '../../models/drawing';
+import { DatabaseService } from '../services/DB.service';
+
+// tslint:disable:no-any
+const HTTP_STATUS_OK = 200;
+let array = [new Drawing('name',['1','2'],'allo',['','',],1,1,[1,1,1])];
+describe('galleryController', () => {
+    let filehandler: Stubbed<FileHandler>;
+    let database: Stubbed<DatabaseService>;
+    let app: Express.Application;
+    beforeEach(async () => {
+        const [container, sandbox] = await testingContainer();
+
+        container.unbind(Types.FileHandler);
+        container.bind(Types.FileHandler).toConstantValue({
+            checkAllDrawingsAreInServer: sandbox.stub().resolves(undefined),
+            //exportDrawing:sandbox.stub(undefined),
+            deleteDrawing: sandbox.stub(undefined),
+        });
+        filehandler = container.get(Types.FileHandler);
+
+        container.unbind(Types.DatabaseService);
+        container.bind(Types.DatabaseService).toConstantValue({
+            getAllDrawingsDb: sandbox.stub().resolves(array),
+            deleteDrawingDb: sandbox.stub().resolves(undefined),
+        });
+        database = container.get(Types.DatabaseService);
+        app = container.get<Application>(Types.Application).app;
+
+    });
+
+    it('should return message from galleryController on valid get request ', async () => {
+        expect(database.getAllDrawingsDb.called);
+        expect(filehandler.checkAllDrawingsAreInServer().called);
+        return supertest(app)
+            .get('/gallery')
+            .expect(HTTP_STATUS_OK)
+            .then((response: any) => {
+                expect('Content-Type', 'Array <Drawing>');
+            });
+    });
+
+    it('should return message from galleryController on valid delete request ', async () => {
+        
+        expect(database.deleteDrawingDb.called)
+        expect(filehandler.deleteDrawing.called);
+        return supertest(app)
+            .delete('/gallery')
+            .expect(HTTP_STATUS_OK)
+            .then((response: any) => {
+                expect(response.body.status).to.equal('200');
+            });
+    });
+
+    
+});
---
+++ server/app/controllers/savedrawing.controller.spec.ts
@@ -0,0 +1,72 @@
+
+import { expect } from 'chai';
+import * as supertest from 'supertest';
+import { Stubbed, testingContainer } from '../../../server/test/test-utils';//Stubbed,Stubbed,
+import { Application } from '../app';
+import { FileHandler } from '../services/file-handler.service';
+import {DatabaseService} from '../services/DB.service';
+import Types from '../types';
+//import {Drawing} from '../../models/drawing'
+import { ObjectID } from 'mongodb';
+//import { Message } from '../../../common/communication/message';
+//import { SaveDrawingController } from './savedrawing.controller';
+//import { request } from 'express';
+//import { ObjectId } from 'mongodb';
+
+
+// tslint:disable:no-any
+const HTTP_STATUS_OK = 200;
+const array = [new ObjectID(12345)];
+
+describe('saveDrawingController', () => {
+    //const baseMessage = { title: 'Image sauvegardée avec succès!', body: '' } as Message;
+    let filehandlerService: Stubbed<FileHandler>;
+    let dbService: Stubbed <DatabaseService>;
+    let app: Express.Application;
+    // spy comme sur cote client sur savedrawing
+    beforeEach(async () => {
+        const [container, sandbox] = await testingContainer();
+
+        container.unbind(Types.DatabaseService);
+        container.bind(Types.DatabaseService).toConstantValue({
+            addDrawingDb: sandbox.stub().resolves(undefined),
+            getIdsOfDrawing: sandbox.stub().resolves(array),
+        });
+
+        container.unbind(Types.FileHandler);
+        container.bind(Types.FileHandler).toConstantValue({
+            saveDrawing: sandbox.stub().resolves(undefined),
+        });
+
+        filehandlerService = container.get(Types.FileHandler);
+        dbService = container.get(Types.DatabaseService);
+        app = container.get<Application>(Types.Application).app;
+
+    });
+
+    it('should return message from saveDrawing service on valid post request ', async () => {
+
+        //const aboutMessage = {baseMessage};
+        expect(dbService.addDrawingDb().called);
+        expect(filehandlerService.saveDrawing().called);
+        return supertest(app)
+            .post('/savedrawing')
+            .expect(HTTP_STATUS_OK)
+            .then((response: any) => {
+                console.log(response.body);
+                expect(response.body.status).to.equal('200');
+                
+            });
+    });
+
+    it('should return error message from saveDrawing service on non-valid post request ', async () => {
+        //expect(dbService.addDrawingDb().called);
+        dbService.addDrawingDb.rejects();
+        return supertest(app)
+            .post('/savedrawing')
+            .expect(400)
+            .then((response: any) => {
+                expect(response.body.status).to.equal('400');
+            });
+    });
+});
