11c0af7299046fe81a48cf01ffdd9157f4c4f2eb
Souque <guillaume.souque@polymtl.ca>
2020-04-08 22:47:46 -0400

    (D) Ajout de l'algorithme de remplissage et de la création de l'élément SVG

--- client/src/app/services/tools/bucket/bucket.service.ts
+++ client/src/app/services/tools/bucket/bucket.service.ts
@@ -3,34 +3,107 @@ import { Injectable, RendererFactory2 } from '@angular/core';
 import { Tool } from 'src/app/models/tool';
 import { DrawState } from 'src/app/state/draw-state';
 import { DrawStore } from 'src/app/store/draw-store';
+import { Coordinate } from 'src/app/models/coordinate';
 
-
+const MAXIMUM_TOLERANCE = 100;
 @Injectable({
   providedIn: 'root'
 })
-export class BucketService extends Tool{
+export class BucketService extends Tool {
+  SVGWidth: number;
+  SVGHeight: number;
 
   constructor(private store: DrawStore, rendererFactory: RendererFactory2) {
     super();
     this.store.stateObs.subscribe((value: DrawState) => {
         this.state = value;
     });
-    this.mouseMoveListener = this.continue.bind(this);
-    this.mouseUpListener = this.stop.bind(this);
 
     this.renderer = rendererFactory.createRenderer(null, null);
   }
 
   start(event: MouseEvent): void {
-
-  }
-
-  continue(event: MouseEvent): void {
-
+    this.svg = this.renderer.createElement('path', 'svg');
+    this.renderer.setAttribute(this.svg, 'stroke-width', '1');
+    this.renderer.setAttribute(this.svg, 'fill', this.state.colorState.firstColor.hex());
+    this.renderer.setAttribute(this.svg, 'stroke', this.state.colorState.firstColor.hex());
+    this.renderer.setAttribute(this.svg, 'stroke-linecap', 'square');
+    this.renderer.setAttribute(this.svg, 'stroke-linejoin', 'square');
+    this.renderer.appendChild(this.state.svgState.drawSvg, this.svg);
+    this.colorArea(event.offsetX, event.offsetY);
+    this.stop();
   }
 
   stop(): void {
-    this.state.svgState.drawSvg.removeEventListener('mousemove', this.mouseMoveListener);
-    this.state.svgState.drawSvg.removeEventListener('mouseup', this.mouseUpListener);
+    this.store.pushSvg(this.svg);
+    this.renderer.removeChild(this.state.svgState.drawSvg, this.svg);
+    this.stopSignal();
+  }
+
+  colorArea(mouseX: number, mouseY: number): void {
+    if (this.state.tolerance === MAXIMUM_TOLERANCE) {
+      this.fillEntireSVG();
+    } else {
+      const unverifiedPixels: Coordinate[] = [];
+      const verifiedPixels: Coordinate[] = [];
+      const pixelsToColor: Coordinate[] = [];
+      unverifiedPixels.push(new Coordinate(mouseX, mouseY));
+      const color = this.getPixelColor(mouseX, mouseY);
+
+      while (unverifiedPixels.length > 0) {
+        const currentPixel = unverifiedPixels.pop();
+        if (currentPixel !== undefined) {
+          const currentPixelColor = this.getPixelColor(currentPixel.pointX, currentPixel.pointY);
+          if (currentPixel.pointX >= 0 && currentPixel.pointX <= 100 && currentPixel.pointY >= 0 && currentPixel.pointY <= 100) { // TODO: get svg width and height
+            if (!verifiedPixels.includes(currentPixel) && this.checkColor(currentPixelColor, color, this.state.tolerance)) {
+              verifiedPixels.push(currentPixel);
+              pixelsToColor.push(currentPixel);
+
+              unverifiedPixels.push(new Coordinate(currentPixel.pointX + 1, currentPixel.pointY));
+              unverifiedPixels.push(new Coordinate(currentPixel.pointX - 1, currentPixel.pointY));
+              unverifiedPixels.push(new Coordinate(currentPixel.pointX, currentPixel.pointY + 1));
+              unverifiedPixels.push(new Coordinate(currentPixel.pointX, currentPixel.pointY - 1));
+            }
+          }
+        }
+      }
+
+      this.renderer.setAttribute(this.svg, 'd', this.pointsToString(pixelsToColor));
+    }
+  }
+
+  createHTMLCanvas(): void {
+
+  }
+
+  fillCanvas(): void {
+
+  }
+
+  getPixelColor(posX: number, posY: number): number[] {
+    return [1,1,1];
+  }
+
+  checkColor(pixelColor: number[], primaryColor: number[], tolerance: number): boolean {
+    const diff = (255 + tolerance) / 100;
+    const isRedOk = (primaryColor[0] >= pixelColor[0] - diff) && (primaryColor[0] <= pixelColor[0] + diff);
+    const isGreenOk = (primaryColor[1] >= pixelColor[1] - diff) && (primaryColor[1] <= pixelColor[1] + diff);
+    const isBlueOk = (primaryColor[2] >= pixelColor[2] - diff) && (primaryColor[2] <= pixelColor[2] + diff);
+    return isRedOk && isGreenOk && isBlueOk;
+  }
+
+  fillEntireSVG(): void {
+    const height = 10; // TODO : get SVG height
+    const width = 10; // TODO : get SVG width
+    const path = `M0 0 L${width} 0 L${width} ${height} L0 ${height} Z`;
+    this.renderer.setAttribute(this.svg, 'd', path);
+  }
+
+  pointsToString(points: Coordinate[]): string {
+    let result = '';
+    for (let point of points) {
+        result = result.concat(`M ${point.pointX} ${point.pointY} h 1`);
+    }
+    return result;
   }
 }
