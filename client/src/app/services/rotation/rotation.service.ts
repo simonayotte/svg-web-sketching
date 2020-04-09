5564b56c552918ee1a89d801c970e60696ae0b9f
Simon <simonayotte17@gmail.com>
2020-04-09 17:43:01 -0400

    (D) rotation avec getBoundingBox

--- client/src/app/services/rotation/rotation.service.ts
+++ client/src/app/services/rotation/rotation.service.ts
@@ -2,7 +2,6 @@ import { Injectable, RendererFactory2 } from '@angular/core';
 import { Tool } from 'src/app/models/tool';
 import { DrawState } from 'src/app/state/draw-state';
 import { DrawStore } from 'src/app/store/draw-store';
-import { SelectionButtons } from 'src/app/models/enums';
 
 export const DEFAULT_ROTATION = 15;
 export const ALT_ROTATION = 2;
@@ -20,7 +19,6 @@ export class RotationService extends Tool {
       this.store.stateObs.subscribe((value: DrawState) => {
           this.state = value;
       });
-      this.isShiftDown = false;
       this.angle = DEFAULT_ROTATION;
 
       this.mouseWheelListener = this.start.bind(this);
@@ -33,30 +31,48 @@ export class RotationService extends Tool {
     event.preventDefault();
     this.state.selectionBox.isRotating = true;
     this.angle = (event.altKey ? ALT_ROTATION : DEFAULT_ROTATION);
-    //event.altKey ? console.log('ALT PRESSED') : console.log('ALT NOT PRESSED');
-    this.rotateSvgs();
-    console.log(this.state.selectionBox.svgs[0].getAttribute('transform'));
+    event.shiftKey ? this.rotateSvg(event.offsetX, event.offsetY) : this.rotateSvgs();
+    // Add logic for undo redo
   }
 
   rotateSvgs(): void {
-    const centerX = this.state.selectionBox.getCenter().pointX;
-    const centerY = this.state.selectionBox.getCenter().pointY;
     for (const svg of this.state.selectionBox.svgs) {
-      
+      const centerX = this.state.selectionBox.getCenter().pointX;
+      const centerY = this.state.selectionBox.getCenter().pointY;
+      console.log(centerX);
+      console.log(centerY);
       let rotation = Tool.getRotation(svg);
       this.renderer.setAttribute(svg, 'transform', `rotate(${(this.angle + rotation) % 360},${centerX},${centerY})`);
       this.state.selectionBox.update();
     }
   }
 
+  // Find closest element to x, y mouse cursor and return svg element inside the box
+  rotateSvg(x: number, y: number): void {
+    let elementToRotate = this.state.selectionBox.svgs[this.findElementToRotate(x, y)];
+    let area = elementToRotate.getBoundingClientRect();
+    const centerX = Math.abs((area.right - area.left)/2);
+    const centerY = Math.abs((area.bottom - area.top)/2);
+    let rotation = Tool.getRotation(elementToRotate);
+    this.renderer.setAttribute(elementToRotate, 'transform', `rotate(${(this.angle + rotation) % 360},${centerX},${centerY})`);
+    this.state.selectionBox.update();
+  }
+
   stop(): void {
     this.state.selectionBox.isRotating = false;
     this.state.svgState.drawSvg.removeEventListener('wheel', this.mouseWheelListener);
   }
-
-  handleKeyDown(key: string): void {
-    if (key === SelectionButtons.Shift) {
-      this.isShiftDown = !this.isShiftDown;
+  // Give MouseEvent coordiante -> finds closest SVG element inside the selection box
+  findElementToRotate(x: number, y:number): number {
+    let currentIndex = 0;
+    for (const svg of this.state.selectionBox.svgs) {
+      const area = svg.getBoundingClientRect();
+      const insideX = (area.left <= x && x <= area.right);
+      const insideY = (area.top <= y && y <= area.bottom);
+      if(insideX && insideY)
+        return currentIndex;
+      currentIndex++;
     }
+    return 0;
   }
 }
